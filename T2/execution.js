194bbb966b949db5527110a270bef0dbce4f87a6
Thiago Cabral <thiago.vale.cabral@gmail.com>
2025-02-03 00:14:13 -0300

    feat: Finalizando o trabalho

--- T2/execution.js
+++ T2/execution.js
@@ -1,23 +1,84 @@
 import * as THREE from 'three';
 import KeyboardState from '../libs/util/KeyboardState.js'
+import Stats from '../build/jsm/libs/stats.module.js';
 import {
     initRenderer,
-    initDefaultBasicLight,
     onWindowResize,
     getMaxSize,
 } from "../libs/util/util.js";
+import GUI from '../libs/util/dat.gui.module.js'
 import { GLTFLoader } from '../build/jsm/loaders/GLTFLoader.js';
 import { OrbitControls } from '../build/jsm/controls/OrbitControls.js';
 import { PointerLockOrbitControls } from './util/PointerLockOrbitControls.js';
 import { VoxelTransformer } from './components/VoxelTransformer.js';
-import { VOXEL_SIZE, EXEC_AXIS_VOXEL_COUNT, MATERIAL, TREE_SLOTS, TREE } from './global/constants.js';
+import { VOXEL_SIZE, EXEC_AXIS_VOXEL_COUNT, MATERIAL, TREE } from './global/constants.js';
 import { VoxelMaterial } from './components/material.js';
 import createPerlin from './util/perlin.js'
+
+var stats = new Stats();
+
+function createAmbientLight(power) {
+    const ambientLight = new THREE.HemisphereLight(
+        'white', // bright sky color
+        'darkslategrey', // dim ground color
+        0.1 * power, // intensity
+    );
+    scene.add(ambientLight);
+}
+
+let characterMesh;
+
+function createDirectionalLight(power = Math.PI) {
+    const mainLight = new THREE.DirectionalLight('white', 1 * power);
+    mainLight.position.copy(new THREE.Vector3(20 * VOXEL_SIZE, 100 * VOXEL_SIZE, 20 * VOXEL_SIZE));
+    mainLight.castShadow = true;
+    scene.add(mainLight);
+
+    updateShadow(mainLight, 20);
+
+    return mainLight;
+}
+
+function updateShadow(lightSource, scale) {
+    if (characterMesh) {
+        lightSource.target = characterMesh
+        lightSource.position.set(
+            characterMesh.position.x + 20 * VOXEL_SIZE,
+            characterMesh.position.y + 100 * VOXEL_SIZE,
+            characterMesh.position.z,
+        );
+    }
+
+    const shadow = lightSource.shadow;
+
+    const shadowSide = 100 * VOXEL_SIZE;
+    const shadowNear = 0.1 * VOXEL_SIZE;
+    const shadowFar = 300 * VOXEL_SIZE;
+
+    shadow.mapSize.width = 2048 * 2 * VOXEL_SIZE;
+    shadow.mapSize.height = 2048 * 2 * VOXEL_SIZE;
+    shadow.camera.near = shadowNear;
+    shadow.camera.far = shadowFar;
+    shadow.camera.left = -shadowSide / 2;
+    shadow.camera.right = shadowSide / 2;
+    shadow.camera.bottom = -shadowSide / 2;
+    shadow.camera.top = shadowSide / 2;
+
+    shadow.needsUpdate = true;
+}
+
+function createLight() {
+    const power = Math.PI;
+    createAmbientLight(power)
+
+    return createDirectionalLight(power)
+}
+
 
 let scene, renderer, light, keyboard;
 scene = new THREE.Scene();    // Create main scene
 renderer = initRenderer("#add9e6");    // View function in util/utils
-light = initDefaultBasicLight(scene);
+light = createLight(scene);
 keyboard = new KeyboardState();
 
 const terrainHeightPerlin = createPerlin();
@@ -25,12 +86,15 @@ const terrainTypePerlin = createPerlin();
 
 const collidables = [];
 
-// scene.fog = new THREE.Fog( 0xcccccc, 10, 100);
+const fog = new THREE.Fog(0xcccccc);
+scene.fog = fog;
 
 function createBatchVoxel(matKey, count) {
     const voxelGeometry = new THREE.BoxGeometry(VOXEL_SIZE, VOXEL_SIZE, VOXEL_SIZE);
     const voxelMeshMaterial = VoxelMaterial.getMeshMaterial(matKey);
     const instancedMesh = new THREE.InstancedMesh(voxelGeometry, voxelMeshMaterial, count);
+    instancedMesh.castShadow = true;
+    instancedMesh.receiveShadow = true;
     scene.add(instancedMesh);
     return instancedMesh;
 }
@@ -39,6 +103,8 @@ function createVoxel(x, y, z, key) {
     const voxelGeometry = new THREE.BoxGeometry(VOXEL_SIZE, VOXEL_SIZE, VOXEL_SIZE);
     const voxelMeshMaterial = VoxelMaterial.getMeshMaterial(key);
     const voxelMesh = new THREE.Mesh(voxelGeometry, voxelMeshMaterial);
+    voxelMesh.castShadow = true;
+    voxelMesh.receiveShadow = true;
 
     voxelMesh.position.set(x, y, z);
     collidables.push(new THREE.Box3().setFromObject(voxelMesh));
@@ -128,16 +194,17 @@ function renderValley() {
 
             const treeRandom = (1 - Math.random()) * 100;
             if (
-                treeRandom <= 1 &&
+                treeRandom <= 0.5 &&
                 selectedPayload.key === MATERIAL.GRASS &&
                 Math.floor(heightValue) > 3
             ) {
                 const nextTreeMatrix = new THREE.Vector3(x, Math.floor(heightValue) + 1, z);
+                const isAwayFromCenter = (x < 20 || x > 20) && (z < 20 || z > 20);
                 const hasTreeNearby = treePositions.some(position =>
                     Math.abs(x - position.x) < 10 &&
                     Math.abs(z - position.z) < 10
                 );
-                if (!hasTreeNearby) {
+                if (!hasTreeNearby && isAwayFromCenter) {
                     treePositions.push(nextTreeMatrix);
                 }
             }
@@ -178,7 +245,8 @@ function renderValley() {
 const camUp = new THREE.Vector3(0.0, 1.0, 0.0);
 
 const orbitalCamera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 2000);
-orbitalCamera.position.copy(new THREE.Vector3(20 * VOXEL_SIZE, 20 * VOXEL_SIZE, 46 * VOXEL_SIZE));
+
+orbitalCamera.position.copy(new THREE.Vector3(150 * VOXEL_SIZE, 30 * VOXEL_SIZE, 150 * VOXEL_SIZE));
 orbitalCamera.up.copy(camUp);
 orbitalCamera.lookAt(new THREE.Vector3(0.0, 0.0, 0.0));
 const orbitalControls = new OrbitControls(orbitalCamera, renderer.domElement);
@@ -189,11 +257,10 @@ firstPersonCamera.position.set(characterPosition.x - (5 * VOXEL_SIZE), character
 firstPersonCamera.up.copy(camUp);
 firstPersonCamera.lookAt(characterPosition);
 const firstPersonControls = new PointerLockOrbitControls(firstPersonCamera, renderer.domElement);
-firstPersonControls.enableZoom = false;
+firstPersonControls.enableZoom = true;
 firstPersonControls.enablePan = false;
 firstPersonControls.maxPolarAngle = Math.PI / 2;
 
-let characterMesh;
 let characterAnimationMixer = Array();
 
 loadGLTFFile('./assets/character/steve.glb', characterPosition);
@@ -216,6 +283,9 @@ function loadGLTFFile(modelName, position) {
         characterMesh = obj;
         firstPersonControls.target.copy(characterMesh.position);
         scene.add(obj);
+
+        isFirstPersonCamera = true;
+        firstPersonControls.lock();
 
         // Create animationMixer and push it in the array of mixers
         var mixerLocal = new THREE.AnimationMixer(obj);
@@ -241,26 +311,19 @@ window.addEventListener('keydown', (event) => {
             firstPersonControls.lock();
             orbitalControls.enabled = false;
             firstPersonControls.enabled = true;
+            scene.fog = fog;
         } else {
             firstPersonControls.unlock();
             orbitalControls.enabled = true;
             firstPersonControls.enabled = false;
             // Isso é necessário para evitar um estado intermediário onde o usuário não consegue orbitar a câmera até apertar a tecla ESC.
             document.exitPointerLock();
+            scene.fog = null;
+            blocker.style.display = 'none';
+            isPaused = false;
         }
     }
 });
-
-document.addEventListener('pointerlockchange', () => {
-    isFirstPersonCamera = firstPersonControls.isLocked;
-    if (firstPersonControls.isLocked) {
-        firstPersonControls.lock();
-        firstPersonControls.enabled = true;
-    } else {
-        orbitalControls.enabled = true;
-        firstPersonControls.enabled = false;
-    }
-})
 
 // movement controls
 const speed = 20;
@@ -268,9 +331,9 @@ let moveForward = false;
 let moveBackward = false;
 let moveLeft = false;
 let moveRight = false;
-let moveUp = false;
 const acceleration = - 10 * VOXEL_SIZE;
 let velocity = 0;
+let canJump = true;
 
 window.addEventListener('keydown', (event) => movementControls(event.key, true));
 window.addEventListener('keyup', (event) => movementControls(event.key, false));
@@ -279,6 +342,29 @@ document.getElementById('webgl-output').addEventListener('click', function () {
         firstPersonControls.lock();
     }
 }, false);
+
+const blocker = document.getElementById('blocker');
+
+blocker.addEventListener('click', function (event) {
+    firstPersonControls.lock();
+    orbitalControls.enabled = false;
+    firstPersonControls.enabled = true;
+    scene.fog = fog;
+});
+
+let isPaused = false;
+
+firstPersonControls.addEventListener('lock', function () {
+    blocker.style.display = 'none';
+    isPaused = false;
+});
+
+firstPersonControls.addEventListener('unlock', function () {
+    if (isFirstPersonCamera) {
+        blocker.style.display = 'block';
+        isPaused = true;
+    }
+});
 
 function movementControls(key, value) {
     switch (key) {
@@ -295,11 +381,11 @@ function movementControls(key, value) {
             moveRight = value;
             break; // D
         case ' ':
-            if (!moveUp) {
+            if (canJump && firstPersonControls.isLocked) {
                 // Se falso, significa que a gente precisa setar a velocidade inicial da altura;
                 velocity = 5 * VOXEL_SIZE;
+                canJump = false;
             }
-            moveUp = value;
             break; // Space
     }
 }
@@ -310,23 +396,23 @@ function adjustQuaternionAxis() {
     euler.setFromQuaternion(firstPersonCamera.quaternion);
 
     // Apply only the Y rotation to the character
-    characterMesh.rotation.set(0, euler.y + Math.PI, 0); // + Math.PI to face opposite direction
+    characterMesh?.rotation.set(0, euler.y + Math.PI, 0); // + Math.PI to face opposite direction
 }
 
 function moveCharacterForward(distance) {
     const direction = new THREE.Vector3(0, 0, -1); // Default forward direction in local space
     const quaternion = new THREE.Quaternion();
-
+    
     // Extract rotation from the camera
     firstPersonCamera.getWorldQuaternion(quaternion);
-
+    
     // Rotate the direction by the camera's quaternion
     direction.applyQuaternion(quaternion);
-
+    
     // Move only in the XZ plane (ignore Y)
     direction.y = 0;
     direction.normalize();
-
+    
     // Apply movement
     characterMesh.position.addScaledVector(direction, distance);
     firstPersonControls.target.copy(characterMesh.position);
@@ -336,17 +422,17 @@ function moveCharacterForward(distance) {
 function moveCharacterRight(distance) {
     const direction = new THREE.Vector3(1, 0, 0); // Default forward direction in local space
     const quaternion = new THREE.Quaternion();
-
+    
     // Extract rotation from the camera
     firstPersonCamera.getWorldQuaternion(quaternion);
-
+    
     // Rotate the direction by the camera's quaternion
     direction.applyQuaternion(quaternion);
-
+    
     // Move only in the XZ plane (ignore Y)
     direction.y = 0;
     direction.normalize();
-
+    
     // Apply movement
     characterMesh.position.addScaledVector(direction, distance);
     firstPersonControls.target.copy(characterMesh.position);
@@ -448,13 +534,14 @@ function moveAnimate(delta) {
         characterAnimationMixer[0].update(delta);
     }
 
-    velocity = velocity + acceleration * delta;
-    const distanceY = velocity * delta;
-    if (!checkCollisionUp(distanceY)) {
-        moveCharacterUp(distanceY);
-    } else {
-        velocity = 0;
-    }
+    // velocity = velocity + acceleration * delta;
+    // const distanceY = velocity * delta;
+    // if (!checkCollisionUp(distanceY)) {
+    //     moveCharacterUp(distanceY);
+    // } else {
+    //     velocity = 0;
+    //     canJump = true;
+    // }
 
     if (moveForward && !checkCollisionForward(distance)) {
         moveCharacterForward(distance);
@@ -470,18 +557,61 @@ function moveAnimate(delta) {
     }
 }
 
+function moveJump(delta) {
+    velocity = velocity + acceleration * delta;
+    const distanceY = velocity * delta;
+    if (!checkCollisionUp(distanceY)) {
+        moveCharacterUp(distanceY);
+    } else {
+        velocity = 0;
+        canJump = true;
+    }
+}
+
 renderValley().then(() => {
     // Esperamos carregar todas as árvores antes de renderizar o mapa.
     render();
 });
 
+let shadowScale = 20;
+
+function buildInterface() {
+    fog.near = shadowScale * VOXEL_SIZE;
+    fog.far = fog.near + 2 * VOXEL_SIZE;
+    var controls = new function () {
+        this.fog = shadowScale;
+
+        this.changeFog = function () {
+            shadowScale = this.fog;
+            updateShadow(light, shadowScale);
+            fog.near = shadowScale * VOXEL_SIZE;
+            fog.far = fog.near + 2 * VOXEL_SIZE;
+        };
+    };
+
+    // GUI interface
+    var gui = new GUI();
+    gui.add(controls, 'fog', 20, 100)
+        .onChange(function (e) { controls.changeFog() })
+        .name("Fog");
+}
+
 const clock = new THREE.Clock();
+buildInterface();
 function render() {
+    stats.update(); // Update FPS
     requestAnimationFrame(render);
 
-    if (firstPersonControls.isLocked) {
-        moveAnimate(clock.getDelta());
+    const delta = clock.getDelta()
+    if (firstPersonControls.isLocked && characterMesh && !isPaused) {
+        moveAnimate(delta);
+    }
+    
+    if (characterMesh) {
+        moveJump(delta);
+        updateShadow(light, shadowScale)
     }
 
     renderer.render(scene, isFirstPersonCamera ? firstPersonCamera : orbitalCamera);
+
 }
