78024f558c93d71146e61a7ea318d3d1533ef5b0
Rayane Oliveira <rayanecontato1@gmail.com>
2025-03-08 03:28:44 -0300

    Adiciona texturas

--- T3/components/material.js
+++ T3/components/material.js
@@ -3,15 +3,23 @@ import { MATERIAL } from "../global/constants.js";
 
 export const VoxelMaterial = {
     catalog: {
+        [MATERIAL.GRASS]: {  textureTop : new THREE.TextureLoader().load('./assets/textures/full_grass.png'),
+                             textureSide : new THREE.TextureLoader().load('./assets/textures/grass_side.jpg')},
+        [MATERIAL.WATER]: {  textureTop : new THREE.TextureLoader().load('./assets/textures/water.jpeg'),
+                             textureSide : new THREE.TextureLoader().load('./assets/textures/water.jpeg')},
         [MATERIAL.DEBUG]: { color: 'red' },
         [MATERIAL.STONE]: { color: 'gray' },
-        [MATERIAL.GRASS]: { color: 'green' },
         [MATERIAL.SAND]: { color: 'gold' },
-        [MATERIAL.M1]: { color: '#D6B99B' },
-        [MATERIAL.M2]: { color: '#A67C52' },
-        [MATERIAL.M3]: { color: '#6E4B3A' },
-        [MATERIAL.M4]: { color: '#F0C35B' },
-        [MATERIAL.M5]: { color: 'green' },
+        [MATERIAL.M1]: {  textureTop : new THREE.TextureLoader().load('./assets/textures/tree_trunk.jpg'),
+                           textureSide : new THREE.TextureLoader().load('./assets/textures/tree_trunk.jpg')},
+        [MATERIAL.M2]: {  textureTop : new THREE.TextureLoader().load('./assets/textures/tree_trunk.jpg'),
+            textureSide : new THREE.TextureLoader().load('./assets/textures/tree_trunk.jpg')},
+        [MATERIAL.M3]: {  textureTop : new THREE.TextureLoader().load('./assets/textures/tree_trunk.jpg'),
+            textureSide : new THREE.TextureLoader().load('./assets/textures/tree_trunk.jpg')},
+        [MATERIAL.M4]: {  textureTop : new THREE.TextureLoader().load('./assets/textures/tree_leaf.png'),
+            textureSide : new THREE.TextureLoader().load('./assets/textures/tree_leaf.png')},
+        [MATERIAL.M5]: {  textureTop : new THREE.TextureLoader().load('./assets/textures/tree_leaf.png'),
+            textureSide : new THREE.TextureLoader().load('./assets/textures/tree_leaf.png')},
         [MATERIAL.BUILDER_FLOOR]: { color: 'lightblue' },
         [MATERIAL.EXEC_FLOOR_0]: { color: 'lightgreen' },
         [MATERIAL.EXEC_FLOOR_1]: { color: '#D2B48C' },
@@ -24,6 +32,19 @@ export const VoxelMaterial = {
         return new THREE.MeshLambertMaterial({ color: "black", wireframe: true });
     },
     getMeshMaterial: (key) => {
-        return new THREE.MeshLambertMaterial({ ...VoxelMaterial.catalog[key] });
+        const textures = VoxelMaterial.catalog[key];
+        console.log(textures)
+        if (textures) {
+            const materials = [
+                new THREE.MeshStandardMaterial({ map: textures.textureSide }), // lado 1
+                new THREE.MeshStandardMaterial({ map: textures.textureSide }), // lado 2
+                new THREE.MeshStandardMaterial({ map: textures.textureTop }), // topo
+                new THREE.MeshStandardMaterial({ map: textures.textureSide }), // base
+                new THREE.MeshStandardMaterial({ map: textures.textureSide }), // lado 3
+                new THREE.MeshStandardMaterial({ map: textures.textureSide })  // lado 4
+            ];
+            return materials;
+        }
     },
-};
+}
+
